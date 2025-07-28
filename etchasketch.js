b101c7f861700cf063ac0029497bfd2e2628e051
jtieu7268 <77133493+jtieu7268@users.noreply.github.com>
2025-07-28 13:03:14 -0400

    Create webpage with 4x4 grid
    The grid is centered in the middle of the page. The size of the grid is based on the width of the screen and thus, the grid displays properly (on one screen) when the width is the smaller dimension of the page. The squares are blue and with white borders for display purposes.

---
+++ etchasketch.js
@@ -0,0 +1,21 @@
+const INITALGRIDDIM = 4;
+
+// const body = document.querySelector("body");
+const container = document.querySelector(".container");
+// TODO: 
+// set min of height or width to be static container dimension 
+// so grid displays completely on screen
+
+let rows = [];  // contains row elements
+let squares = [];   // contains square elements arranged in 2d array grid
+for (let i = 0; i < INITALGRIDDIM; i++) {
+    rows.push(document.createElement("div"));
+    rows[i].className = "row";
+    squares.push([]);
+    for (let j = 0; j < INITALGRIDDIM; j++) {
+        squares[i].push(document.createElement("div"));
+        squares[i][j].className = "square";
+    }
+    rows[i].append(...squares[i]);
+}
+container.append(...rows);
