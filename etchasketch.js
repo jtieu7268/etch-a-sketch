4c545177f3813c575a570f73001b97e1d7ff2ff6
jtieu7268 <77133493+jtieu7268@users.noreply.github.com>
2025-07-28 14:08:42 -0400

    Add a button on top of the screen that will ask user for new grid size and create new grid
    The button will continue to prompt the user if a number greater than 100 is entered to prevent potential delays, freezing, or crashing.

--- etchasketch.js
+++ etchasketch.js
@@ -6,18 +6,37 @@ const container = document.querySelector(".container");
 // set min of height or width to be static container dimension 
 // so grid displays completely on screen
 
-let rows = [];  // contains row elements
-let squares = [];   // contains square elements arranged in 2d array grid
-for (let i = 0; i < INITALGRIDDIM; i++) {
-    rows.push(document.createElement("div"));
-    rows[i].className = "row";
-    squares.push([]);
-    for (let j = 0; j < INITALGRIDDIM; j++) {
-        squares[i].push(document.createElement("div"));
-        squares[i][j].className = "square";
-        squares[i][j].addEventListener('mouseenter', 
-            (event) => event.target.classList.add("colored"));
+let [grid, rows, squares] = createGrid(INITALGRIDDIM);
+
+const newGridButton = document.querySelector("button");
+newGridButton.addEventListener('click', 
+    () => {
+        let newGridDim;
+        while (newGridDim === undefined || newGridDim > 100) {
+            newGridDim = prompt("Enter the number of squares per side for the new grid.\nMax is 100.");
+        }
+        grid.remove();
+        [grid, rows, squares] = createGrid(newGridDim);
+    });
+
+function createGrid (gridDim) {
+    const grid = document.createElement("div");
+    grid.className = "grid";
+    container.appendChild(grid);
+    let rows = [];  // contains row elements
+    let squares = [];   // contains square elements arranged in 2d array grid
+    for (let i = 0; i < gridDim; i++) {
+        rows.push(document.createElement("div"));
+        rows[i].className = "row";
+        squares.push([]);
+        for (let j = 0; j < gridDim; j++) {
+            squares[i].push(document.createElement("div"));
+            squares[i][j].className = "square";
+            squares[i][j].addEventListener('mouseenter', 
+                (event) => event.target.classList.add("colored"));
+        }
+        rows[i].append(...squares[i]);
     }
-    rows[i].append(...squares[i]);
+    grid.append(...rows);
+    return [grid, rows, squares];
 }
-container.append(...rows);
